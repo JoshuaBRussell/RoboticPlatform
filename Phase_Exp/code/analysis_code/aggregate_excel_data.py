3517fca83b413cdb2d6ee75df16d45cbfe67f036
JoshuaBRussell <theruss007@gmail.com>
2021-03-26 01:53:46 -0700

    After producing an Excel file for a particular subject, this will aggregate them all into one file.

---
+++ Phase_Exp/code/analysis_code/aggregate_excel_data.py
@@ -0,0 +1,26 @@
+#Script to aggregate the data summary files produced by the Matlab analysis code
+#into a single sheet. 
+
+import pandas as pd
+from openpyxl import load_workbook
+
+
+OUTPUT_FILE_NAME = "DIR_Summary.xlsx"
+SUBJ_NAMES = ["Carl", "Abhigya", "Vu", "Emily", "Ashley", "Carly", "Lily"]
+
+FILE_NAME_SUFFIX = "_datasummary.xlsx"
+
+for i in range(len(SUBJ_NAMES)):
+    curr_file = SUBJ_NAMES[i]+FILE_NAME_SUFFIX
+    print("Opening: ", curr_file)
+
+    spread_sheet_obj = pd.ExcelFile(curr_file)
+    
+    book = load_workbook(OUTPUT_FILE_NAME)
+    writer = pd.ExcelWriter(OUTPUT_FILE_NAME, mode='w', engine='openpyxl')
+    writer.book = book
+    writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
+
+    spread_sheet_obj.parse().to_excel(writer, index=False,sheet_name = 'Sheet1', startrow=(writer.sheets['Sheet1'].max_row + 1))
+
+    writer.save()
