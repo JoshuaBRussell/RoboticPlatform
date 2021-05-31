8ebb3dea8bbc3f3b7ecb3e239a620db34dadf024
JoshuaBRussell <theruss007@gmail.com>
2021-05-31 14:02:44 -0700

    Refactor find_all_peaks.m for clarity.

--- MS_Study_Trials/code/day1analysis/find_all_peaks.m
+++ MS_Study_Trials/code/day1analysis/find_all_peaks.m
@@ -7,26 +7,26 @@ for j=aa:ab
 [a,b]=findpeaks(-1*dir*emg_data{1,j}.data(:,18));
 sizep=size(a);
 
-for i=1:sizep(1,1)
+    for i=1:sizep(1,1)
  
-    if(and(and(k~=a(i),a(i)~=0),dir>0))
-      actualpeaks(ct,1)=j;
-      actualpeaks(ct,2)=1;
-     actualpeaks(ct,3)=b(i);
-      k=a(i);
-      ct=ct+1;
-    end
+        if(and(and(k~=a(i),a(i)~=0),dir>0))
+          actualpeaks(ct,1)=j;
+          actualpeaks(ct,2)=1;
+          actualpeaks(ct,3)=b(i);
+          k=a(i);
+          ct=ct+1;
+        end
 
         if(and(and(k~=a(i),a(i)~=0),dir<0))
-      actualpeaks(ct,1)=j;
-      actualpeaks(ct,2)=2;
-     actualpeaks(ct,3)=b(i);
-      k=a(i);
-      ct=ct+1;
-    end
+          actualpeaks(ct,1)=j;
+          actualpeaks(ct,2)=2;
+          actualpeaks(ct,3)=b(i);
+          k=a(i);
+          ct=ct+1;
+        end
     end
 end
- a=0;
-  b=0;
+a=0;
+b=0;
 end
 
