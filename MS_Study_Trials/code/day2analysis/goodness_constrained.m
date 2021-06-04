bc7f8245cb9d9afebe427a64b5d16375478deede
JoshuaBRussell <theruss007@gmail.com>
2021-06-04 00:11:20 -0700

    Update the function that finds the %VAF for the constrained version to use the correct regression window.

--- MS_Study_Trials/code/day2analysis/goodness_constrained.m
+++ MS_Study_Trials/code/day2analysis/goodness_constrained.m
@@ -1,7 +1,7 @@
 
 
-vartor1=var(diff_p1_plat_torqueimpm(1120:1320));
-varimp1=var(diff_p1_plat_torqueimpm(1120:1320)-(diff_p1_foot_posm(1120:1320)*p1impm2(1)+diff_p1_foot_velm(1120:1320)*p1impm2(2)+diff_p1_foot_accm(1120:1320)*p1impm2(3)));
+vartor1=var(diff_p1_plat_torqueimpm(REGRESSION_WINDOW_MIN_INDEX:REGRESSION_WINDOW_MAX_INDEX));
+varimp1=var(diff_p1_plat_torqueimpm(REGRESSION_WINDOW_MIN_INDEX:REGRESSION_WINDOW_MAX_INDEX)-(diff_p1_foot_posm(REGRESSION_WINDOW_MIN_INDEX:REGRESSION_WINDOW_MAX_INDEX)*p1impm2(1)+diff_p1_foot_velm(REGRESSION_WINDOW_MIN_INDEX:REGRESSION_WINDOW_MAX_INDEX)*p1impm2(2)+diff_p1_foot_accm(REGRESSION_WINDOW_MIN_INDEX:REGRESSION_WINDOW_MAX_INDEX)*p1impm2(3)));
 goodnessc(1,1)=100*(1-(varimp1/vartor1))
 
 
